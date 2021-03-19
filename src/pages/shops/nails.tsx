1230a01c8e8ee52fdae70f051618a8545b58b6f6
Tagir Almasov <tccup1@gmail.com>
2021-03-19 16:49:06 +0000

    Fixes Tagir-A/exp#21

--- src/pages/shops/nails.tsx
+++ src/pages/shops/nails.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import * as contentful from 'contentful'
+
+const client = contentful.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN!,
+})
 
 export default function NailsPage() {
+  async function fetchEntries(): Promise<any[]> {
+    const entries = await client.getEntries()
+    if (entries.items) return entries.items
+    return []
+    console.log(`Error getting Entries for .`)
+  }
+  const [posts, setPosts] = useState<any[]>([])
+
+  useEffect(() => {
+    async function getPosts() {
+      const allPosts = await fetchEntries()
+      setPosts([...allPosts])
+    }
+    getPosts()
+  }, [])
   return (
     <>
       <Head>
@@ -15,6 +36,9 @@ export default function NailsPage() {
         </Link>
       </h2>
       <div>Best nails here!</div>
+      {posts.length > 0
+        ? posts.map((p) => <div key={p.fields.id}>{p.fields.productName}</div>)
+        : null}
     </>
   )
 }
